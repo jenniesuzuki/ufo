c622c88e4ee3ff44fb186bce5308aa7b2c900da0
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2025-08-15 11:50:00 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/fiap/ufo/UfoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ufo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/ufo/UfoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ufo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
